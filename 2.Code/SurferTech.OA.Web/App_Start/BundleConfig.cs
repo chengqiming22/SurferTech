b459057ca6ebf14b618a235c06afdae58cec5b25
qmcheng <qmcheng@Ctrip.com>
2016-12-27 12:38:07 +0800

    修改布局页样式

--- 2.Code/SurferTech.OA.Web/App_Start/BundleConfig.cs
+++ 2.Code/SurferTech.OA.Web/App_Start/BundleConfig.cs
@@ -20,6 +20,9 @@ namespace SurferTech.OA.Web
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/site").Include(
+                      "~/Scripts/index.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
